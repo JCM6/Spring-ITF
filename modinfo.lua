c08b71716711795b0283f8468156bccc0fc067f3
JCM6 <jcmoody6@gmail.com>
2016-12-11 20:19:42 -0800

    Create modinfo.lua

---
+++ modinfo.lua
@@ -0,0 +1,17 @@
+local modinfo = {
+	name = "Into the Fallowland",
+	shortname = "ITF",
+	game = "Into the Fallowland",
+	shortgame = "Basegame",
+	description = "Base",
+	url = "http://www.somewebsite.com/",
+	
+	modtype = "1",
+
+	depend = {
+		"cursors.sdz",
+		"springcontent.sdz",
+	}
+}
+
+return modinfo
