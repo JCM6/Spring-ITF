8cc873da81bf26ee83cc3e8a3ee5cd7e076533ce
JCM6 <jcmoody6@gmail.com>
2016-12-12 21:51:39 -0800

    Update movedefs.lua

--- gamedata/movedefs.lua
+++ gamedata/movedefs.lua
@@ -1,8 +1,15 @@
 local moveDefs =
 {
 	{
-		name = "Default2x2",
+		name = "Ground2x2",
 		footprintX = 2,
+		maxWaterDepth = 10,
+		maxSlope = 20,
+		crushStrength = 25,
+	},
+	{
+		name = "Ground4x4",
+		footprintX = 4,
 		maxWaterDepth = 10,
 		maxSlope = 20,
 		crushStrength = 25,
